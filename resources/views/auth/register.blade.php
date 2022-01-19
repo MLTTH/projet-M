ab2aa62cee020b992da4757812baa69c0d093d2a
mltth <mltth.dev@gmail.com>
2022-01-19 18:25:02 +0100

    add phone to RegisteredUserController

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,7 @@
             </div>
 
             <!-- Phone -->
-            <div>
+            <div class="mt-4">
                 <x-label for="phone" :value="__('Phone')" />
 
                 <x-input id="phone" class="block mt-1 w-full" type="text" name="phone" :value="old('phone')" required autofocus />
