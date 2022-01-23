1d935c8ec074f6bd7ecd30822118e54f2b111701
mltth <mltth.dev@gmail.com>
2022-01-23 13:17:22 +0100

    custom info card ok

--- resources/views/components/custom-card.blade.php
+++ resources/views/components/custom-card.blade.php
@@ -1,61 +1,62 @@
-<div class="md:h-full md:bg-cover " style="background-image: url('image/illustration_connection.svg');">
+    {{-- TITLE --}}
     <div class="py-12">  
         <h2 class="text-center text-amber-500 text-5xl font-bold">Comment ça marche ?</h2>
     </div>
-    <div>
+
+{{-- CARD-PARENT --}}
+<div class="md:grid grid-cols-2 gap-4">
+
+{{-- CARD1-RIGHT --}}
+<div class="md:h-full bg-cover" style="background-image: url('image/undraw_check.svg');">
 </div>
+{{-- CARD1-LEFT --}}
+<div class="md:relative w-4/5 m-10">
 
-
-
-<div class="md:min-h-screen md:flex md:items-center md:justify-center">
-
-{{-- CARD --}}
-<div class="md:relative w-72 md:m-10">
-{{-- Yellow bg --}}
-            {{-- Yellow line --}}
-            <div class="md:relative shadow-2xl bg-gradient-to-r from-indigo-500 to-indigo-600 text-gray-800 rounded-xl p-8 space-y-7">
-           
-        {{-- % --}}
-        <div class="text-2xl font-bold text-gray-50 ">Je cherche un colocataire</div>
-        <div class="h-2 w-20 bg-yellow-400"></div>
-
-        <div class="text-2xl font-extrabold text-gray-50">01<span class="text-sm font-semibold uppercase">Je m'inscris</span></div>
-        <div class="text-2xl font-extrabold text-gray-50">02<span class="text-sm font-semibold uppercase">Je poste mon annonce</span></div>
-        <div class="text-2xl font-extrabold text-gray-50">03<span class="text-sm font-semibold uppercase">J'attends d'être contacté(e)</span></div>
-
-
-
-        {{-- learn more --}}
-            <a href="#" class="block text-yellow-400 font-bold tracking-wide flex">
+    <div class="md:relative shadow-2xl bg-gradient-to-r from-indigo-500 to-indigo-600 text-gray-800 rounded-xl p-8 space-y-7">
+               
+        <div class="text-2xl font-bold text-gray-50 ">Je cherche une colocation</div>
+        <div class="text-xl font-extrabold border-l-4 border-white"><span class="pl-3 text-sm font-semibold uppercase text-white">Je m'inscris</span></div>
+        <div class="text-xl font-extrabold border-l-4 border-white"><span class="pl-3 text-sm font-semibold uppercase text-white">Je consulte les offres</span></div>
+        <div class="text-xl font-extrabold border-l-4 border-white"><span class="pl-3 text-sm font-semibold uppercase text-white">Je prends contact avec mes futurs colocataires</span></div>
+        
+        {{-- S'inscrire --}}
+            <a href="{{ route('register') }}" class="block text-yellow-400 font-bold tracking-wide flex">
             <span>S'inscrire</span>
             </a>
     </div>
-    </div>
+</div>
 
 
-{{-- CARD --}}
-<div class="lg:relative w-72 md:m-10">
-    {{-- Yellow bg --}}
-                {{-- Yellow line --}}
-            <div class="md:relative shadow-2xl bg-gradient-to-r from-indigo-500 to-indigo-600 text-gray-800 rounded-xl p-8 space-y-7">
-               
-            {{-- % --}}
-            <div class="text-2xl font-bold text-gray-50 ">Je cherche une chambre</div>
-            <div class="h-2 w-20 bg-yellow-400"></div>
+{{-- CARD2 --}}
+
+{{-- CARD1-RIGHT --}}
+<div class="md:h-full bg-cover" style="background-image: url('image/undraw_check.svg');">
+</div>
+{{-- CARD1-LEFT --}}
+</div>
+</div>
+
+
+{{-- CARD-PARENT --}}
+<div class="md:grid grid-cols-2 gap-4">
+    {{-- CARD LEFT --}}
+<div class="lg:relative md:w-4/5 m-10">
     
-            <div class="text-xl font-extrabold text-gray-50">01<span class="text-sm font-semibold uppercase">Je m'inscris</span></div>
-            <div class="text-xl font-extrabold text-gray-50">02<span class="text-sm font-semibold uppercase">Je consulte les offres</span></div>
-            <div class="text-xl font-extrabold text-gray-50">03<span class="text-sm font-semibold uppercase">Je prends contact avec leur propriétaire </span></div>
-    
-    
-    
-            {{-- learn more --}}
-                <a href="#" class="block text-yellow-400 font-bold tracking-wide flex">
+        <div class="md:relative shadow-2xl bg-gradient-to-r from-indigo-500 to-indigo-600 text-gray-800 rounded-xl p-8 space-y-7">
+            <div class="text-2xl font-bold text-gray-50 ">Je propose une colocation</div>
+            <div class="text-xl font-extrabold border-l-4 border-white"><span class="pl-3 text-sm font-semibold uppercase text-white">Je m'inscris</span></div>
+            <div class="text-xl font-extrabold border-l-4 border-white"><span class="pl-3 text-sm font-semibold uppercase text-white">Je poste mon annonce</span></div>
+            <div class="text-xl font-extrabold border-l-4 border-white"><span class="pl-3 text-sm font-semibold uppercase text-white">J'attends d'être contacté par mes futurs colocataires</span></div>
+
+            {{-- S'inscrire --}}
+            <a href="{{ route('register') }}" class="block text-yellow-400 font-bold tracking-wide flex">
                 <span>S'inscrire</span>
-                </a>
+            </a>
         </div>
-        </div>
+</div>
     
-
+    {{-- CARD1-RIGHT --}}
+    <div class="md:h-full bg-cover" style="background-image: url('image/undraw_post.svg');">
+    </div>
 </div>
 </div>
